fffa00a698910c0036a8fb373133d5457ded4421
AndreaGiuntiniUNIFI <49487580+AndreaGiuntiniUNIFI@users.noreply.github.com>
2019-04-10 16:30:10 +0200

    Add files via upload

---
+++ Global.py
@@ -0,0 +1,10 @@
+from kivy.uix.screenmanager import ScreenManager, FadeTransition
+
+# Variabili utlizzate all'interno dell'applicazione
+
+EMAIL = ""
+VIEW = False
+FIELD = ""
+SORT = "desc"
+contacts_filename = 'Data/contacts.dat'
+sm = ScreenManager(transition=FadeTransition())
